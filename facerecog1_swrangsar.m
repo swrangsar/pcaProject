b66915397c31a0c0ae84b0937395c843d87ab524
Swrangsar Basumatary <swrangsar@aol.com>
2012-11-07 02:19:02 +0530

    gotta find an exact match

--- facerecog1_swrangsar.m
+++ facerecog1_swrangsar.m
@@ -9,17 +9,26 @@ plotPath = './results/';
 
 testDATA = orldata_test; % Get test images from orldata
 
-% Case 1: Test using trained image
+%% Case 1: Test using trained image
 % Fill to complete 3.(a)
 % Use one image from the training data set 
 load DATA;
 orlImgTrain = DATA(:, 1); % fill
-h = figure; imshow( reshape(orlImgTrain, 112, 92), []);
-fileName = [plotPath 'orlImgTrain'];
-print(h, '-dtiffn', fileName);
+figure; imshow( reshape(orlImgTrain, 112, 92), []);
+print('-dtiffn', [plotPath 'orlImgTrain']);
 
 % % Reconstruct the above image using the PCs
-% img1_est = ; % fill
+load pcaProj;
+load w; load psi;
+weightVector = pcaProj(:, 1);
+orlImgTrainEst = zeros(size(w (:, 1)));
+for k = 1:length(weightVector)
+    orlImgTrainEst = orlImgTrainEst + (weightVector(k) * (w(:, 1)));
+end
+orlImgTrainEst = orlImgTrainEst + psi;
+figure; imshow( reshape(orlImgTrainEst, 112, 92), []);
+print('-dtiffn', [plotPath 'orlImgTrainEst']);
+
 % img1_err = ; % fill
 % 
 % % Case 2: Test using a test image from orl data base
